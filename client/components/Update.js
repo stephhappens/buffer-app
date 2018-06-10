1ee091c3a2cf18f6c96184b597ba51d33746ba9d
Stephanie Provence <stephanie@lewiscommunications.com>
2018-06-10 16:58:23 -0500

    Rename classNames to allow further customization

--- client/components/Update.js
+++ client/components/Update.js
@@ -21,10 +21,10 @@ class Update extends Component {
           <p className="update-text" dangerouslySetInnerHTML={{ __html: this.props.text_formatted }}></p>
           {this.renderImage(this.props.media)}
         </div>
-        <ul className="analytics">
-          <i class="bi bi-like"></i><li className="analytics__summary -favorites">{this.props.statistics.favorites}</li>
-          <i class="bi bi-click"></i><li className="analytics__summary -clicks">{this.props.statistics.clicks}</li>
-          <i class="bi bi-retweet"></i><li className="analytics__summary -favorites">{this.props.statistics.retweets}</li>
+        <ul className="update-analytics">
+          <i className="bi bi-like"></i><li className="analytics__summary">{this.props.statistics.favorites}</li>
+          <i className="bi bi-click"></i><li className="analytics__summary">{this.props.statistics.clicks}</li>
+          <i className="bi bi-retweet"></i><li className="analytics__summary">{this.props.statistics.retweets}</li>
         </ul>
         <div className="update-info">
           <div>{this.props.day} at {this.props.due_time}</div>
