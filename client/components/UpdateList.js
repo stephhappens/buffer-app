d13db4acdeb1d764bfaaf603eadbfb46b04c527c
Stephanie Provence <stephanie@lewiscommunications.com>
2018-06-10 20:17:35 -0500

    Create constructor and initial state, add TODO to fix data request

--- client/components/UpdateList.js
+++ client/components/UpdateList.js
@@ -3,7 +3,40 @@ import actions from  '../actions';
 import Update from  './Update';
 
 class UpdateList extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      items: [], 
+      visible: 10,
+      error: false
+    };
+
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  loadMore() {
+    this.setState({
+      visible: this.state.visible + 20
+    });
+  }
+
+  componentWillMount() {
+    // TODO: This isn't correct figure out how to grab more data from api
+    $get(Update, (res, status) => {
+      if (status === 'success') {
+        this.setState({
+          items: res,
+          loaded: true
+        });
+      } else {
+        console.log(status);
+        this.setState({
+          error: true
+        });
+      }
+    });
+  }
   render() {
     if (!this.props.updates || !this.props.updates.length) {
       return <p>You have no updates</p>
@@ -16,6 +49,8 @@ class UpdateList extends Component {
             key={idx}
           />
         ))}
+        {this.state.visible < this.state.items.length && 
+        <button className="load-more" onClick={this.loadMore}>Load More!</button>}
       </div>
     )
     return
